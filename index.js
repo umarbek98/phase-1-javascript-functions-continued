b6c7dc76e0dc0b7b5e2e5fbcfa44fd8f2acb7f11
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-01-10 14:30:18 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(result = "*") {
+    const innerFunction = function (type = "a hard worker") {
+        return `You are ${result}${type}${result}!`
+    }
+    return innerFunction;
+}
